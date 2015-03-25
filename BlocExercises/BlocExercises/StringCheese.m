f77d71e189d45312b83a71bfc4b0e55a3bc02e40
Solomon Fernandez <solomonfdez@gmail.com>
2015-03-25 00:57:10 -0700

    Completed Cheese

--- BlocExercises/BlocExercises/StringCheese.m
+++ BlocExercises/BlocExercises/StringCheese.m
@@ -12,19 +12,40 @@
 
 - (NSString *) favoriteCheeseStringWithCheese:(NSString *)cheeseName {
     /* WORK HERE */
-    return nil;
+    NSString *cheeseString = [NSString stringWithFormat:@"My favorite cheese is %@.",cheeseName];
+    return cheeseString;
 }
 
 - (NSString *) cheeseNameWithoutCheeseSuffix:(NSString *)cheeseName {
     /* WORK HERE */
-    return nil;
+    // search for 'cheese'
+    //if found - remove and return result
+
+    NSRange tldr = [cheeseName rangeOfString:@" cheese" options:NSCaseInsensitiveSearch];
+    
+    if (tldr.location != NSNotFound) {
+        //NSLog(@"range of .com: %d, %d", tldr.location, tldr.length);
+        NSString *withoutCheese = [cheeseName stringByReplacingCharactersInRange:tldr withString:@""];
+        //NSLog(@"removed .com, domain is now: %@", domainName);
+        return withoutCheese;
+    }
+    else {
+        return cheeseName;
+    }
+    
 }
 
 - (NSString *) numberOfCheesesStringWithCheeseCount:(NSUInteger)cheeseCount {
     if (cheeseCount == 1) {
         /* WORK HERE, ASSUMING THERE IS 1 CHEESE */
+        // return nubmer + cheese
+        NSString *cheeseString = [NSString stringWithFormat:@"%lu cheese",(unsigned long)cheeseCount];
+        return cheeseString;
     } else {
         /* WORK HERE, ASSUMING THERE ARE 2+ CHEESES */
+        //return number + cheeses
+        NSString *cheeseString = [NSString stringWithFormat:@"%lu cheeses",(unsigned long)cheeseCount];
+        return cheeseString;
     }
     
     /*
