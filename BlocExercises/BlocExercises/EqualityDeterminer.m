d3604618dc7b052d6e5cf067e626b15f205d1032
Solomon Fernandez <solomonfdez@gmail.com>
2015-03-25 15:48:49 -0700

    Rest of Equality

--- BlocExercises/BlocExercises/EqualityDeterminer.m
+++ BlocExercises/BlocExercises/EqualityDeterminer.m
@@ -13,21 +13,38 @@
 - (BOOL) string:(NSString *)string1 isTheSameAsString:(NSString *)string2 {
     /* WORK HERE */
     
-    NSString *cerealString = @"Cheerios";
-    NSString *identicalCerealString = @"Cheerios";
-    BOOL cerealEquality = cerealString == identicalCerealString;
     
-    return cerealEquality;
+
+    
+    
+    return TRUE;
 }
 
 - (BOOL) number:(NSNumber *)number1 isTheSameAsNumber:(NSNumber *)number2 {
     /* WORK HERE */
-    return (number1 == number2);
+    
+    NSNumber *sixNumber = number1;
+    NSNumber *identicalSixNumber = number2;
+    BOOL numberEquality = [sixNumber isEqualToNumber:identicalSixNumber];
+    
+    return numberEquality;
+    
+
 }
 
 - (BOOL) integer:(NSInteger)integer1 isGreaterThan:(NSInteger)integer2 {
     /* WORK HERE */
-    return NO;
+    
+    NSInteger high = integer1;
+    NSInteger low = integer2;
+    
+    BOOL highIsGreater = high > low;
+    
+    return highIsGreater;
+    
 }
 
 @end
+
+
+
